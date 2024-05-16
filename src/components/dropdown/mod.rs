7fcdcd193a58a562f7233d8b9f42a53a43400a93
averichev <averichev@yandex.ru>
2024-05-16 10:21:21 +0300

    chore: Dropdown fix

--- src/components/dropdown/mod.rs
+++ src/components/dropdown/mod.rs
@@ -1,10 +1,9 @@
 use dioxus::prelude::*;
 use wasm_bindgen::JsValue;
 use crate::components::button::Button;
-use crate::components::{Clicked, DarkMode, DocumentClick};
+use crate::components::{Clicked, DarkMode};
 use web_sys::{console, EventTarget};
 use uuid::Uuid;
-use web_sys::js_sys::Atomics::store;
 
 #[derive(PartialEq, Clone, Props)]
 pub struct DropdownProps {
@@ -14,58 +13,22 @@ pub struct DropdownProps {
 #[component]
 pub fn Dropdown(props: DropdownProps) -> Element {
     let uid = use_memo(Uuid::new_v4);
-
     let mut show = use_signal(|| false);
     let mut class = String::from("dropdown-menu");
     if show() {
         class.push_str(" show");
     }
-
-
-    //let click = use_context::<DocumentClick>().target;
-    // use_effect(move || {
-    //     let r = click.read();
-    //     console::log_1(&JsValue::from_str(&format!("Detected change, new clicked ID: {:?}", r.target.read())));
-    //     match r.target.as_ref() {
-    //         None => {
-    //             console::log_1(&JsValue::from_str("нет '"));
-    //             //show.set(false);
-    //         }
-    //         Some(s) => {
-    //             console::log_1(&JsValue::from_str("есть id"));
-    //             console::log_1(&JsValue::from_str(s.as_str()));
-    //             /*if uid.to_string().eq(s.rea) {
-    //                 console::log_1(&JsValue::from_str("равны"));
-    //             }*/
-    //         }
-    //     };
-    // });
-
-    let mut store = DocumentClick::new();
     let dark_mode = use_context::<Signal<DarkMode>>();
     let clicked = use_context::<Signal<Clicked>>();
-
-
     use_effect(move || {
-        console::log_1(&JsValue::from_str(&format!("Detected change, new clicked ID: {:?}", store.value())));
-        if uid.to_string() == clicked().id {
-            console::log_1(&JsValue::from_str("равны"));
-        }
-        else {
+        if uid.to_string() == clicked().id {} else {
             show.set(false);
         }
     });
-
-
     let on_click = move |_| {
-        console::log_1(&JsValue::from_str("button clicked!".to_string().as_str()));
-        ////store.set("fake id".to_string());
         show.toggle();
     };
-    let style = if dark_mode().0 { "дарк" } else { "лайт" };
-    let style2 = store.target();
     let id_clicked = clicked().id;
-
     rsx! {
         div{
             class: "dropdown",
@@ -85,14 +48,5 @@ pub fn Dropdown(props: DropdownProps) -> Element {
                 }
             },
         },
-        div{
-            "store value: {style2}"
-        },
-        div{
-            "мод: {style}"
-        },
-        div{
-            "clicked: {id_clicked}"
-        }
     }
 }
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -12,70 +12,38 @@ use web_sys::{console, EventTarget};
 use dioxus::html::*;
 use uuid::Uuid;
 
-#[modx::store]
-pub struct DocumentClick {
-    pub target: String,
-}
-
-impl DocumentClick {
-    pub fn set(&mut self, target: String){
-        console::log_1(&"set id in store".into());
-        self.target.set(target);
-    }
-    pub fn clear(&mut self){
-        console::log_1(&"clear".into());
-        self.target.set(String::new());
-    }
-    pub fn value(&mut self) -> Signal<String> {
-        console::log_1(&"read value".into());
-        self.target
-    }
-}
-
 #[derive(PartialEq, Clone, Props)]
 pub struct DocumentListenerProps {
     children: Element,
 }
 
-
 #[derive(Clone, Copy)]
 pub struct DarkMode(pub bool);
 
 #[derive(Clone)]
-pub struct Clicked{
-    id: String
+pub struct Clicked {
+    id: String,
 }
 
 #[component]
 pub fn DocumentListener(props: DocumentListenerProps) -> Element {
     use_context_provider(|| Signal::new(DarkMode(false)));
-    let mut clicked = use_context_provider(|| Signal::new(Clicked{ id: "".to_string() }));
-    let mut store = DocumentClick::new();
+    let mut clicked = use_context_provider(|| Signal::new(Clicked { id: "".to_string() }));
     use_effect(move || {
         let document = web_sys::window().unwrap().document().unwrap();
         let closure = Closure::wrap(Box::new(move |evt: web_sys::MouseEvent| {
-            console::log_1(&"document clicked!".into());
-            console::log_1(evt.as_ref());
             let target = evt.target().unwrap();
             let element = target.dyn_into::<web_sys::Element>().unwrap();
             if let Some(data_id) = element.get_attribute("id") {
-                store.set(data_id.clone());
                 clicked.write().id = data_id;
             } else {
-                store.clear();
                 clicked.write().id = String::new();
             }
         }) as Box<dyn FnMut(_)>);
         document.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref()).unwrap();
         closure.forget();
     });
-    use_effect(move || {
-        console::log_1(&JsValue::from_str(&format!("Detected change in DocumentListener, new clicked ID: {:?}", store.value())));
-    });
     rsx! {
         {props.children},
-        div{
-            "store.value: {store.value()}"
-        }
     }
 }
