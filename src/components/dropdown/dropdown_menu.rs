a7bf7ea0e7c30451f4dc3bf80743ed86c8e9e735
averichev <averichev@users.noreply.github.com>
2024-05-16 13:34:41 +0300

    improvement: Dropdown
    * improvement: Separate dropdown
    
    * improvement: Dropdown

---
+++ src/components/dropdown/dropdown_menu.rs
@@ -0,0 +1,28 @@
+use dioxus::prelude::*;
+
+#[derive(PartialEq, Clone, Props)]
+pub struct DropdownMenuProps {
+    show: Option<bool>,
+    children: Option<Element>,
+}
+
+#[component]
+pub fn DropdownMenu(props: DropdownMenuProps) -> Element {
+    let mut class = String::from("dropdown-menu");
+    match props.show {
+        None => {
+            class.push_str(" show");
+        }
+        Some(s) => {
+            if s {
+                class.push_str(" show");
+            }
+        }
+    }
+    rsx! {
+        ul{
+            class,
+            {props.children}
+        },
+    }
+}
--- src/components/dropdown/mod.rs
+++ src/components/dropdown/mod.rs
@@ -1,23 +1,24 @@
+pub mod dropdown_menu;
+
 use dioxus::prelude::*;
 use wasm_bindgen::JsValue;
 use crate::components::button::Button;
 use crate::components::{Clicked, DarkMode};
 use web_sys::{console, EventTarget};
 use uuid::Uuid;
+use crate::components::dropdown::dropdown_menu::DropdownMenu;
 
 #[derive(PartialEq, Clone, Props)]
 pub struct DropdownProps {
     children: Element,
+    menu: Option<Element>
 }
 
 #[component]
 pub fn Dropdown(props: DropdownProps) -> Element {
     let uid = use_memo(Uuid::new_v4);
     let mut show = use_signal(|| false);
-    let mut class = String::from("dropdown-menu");
-    if show() {
-        class.push_str(" show");
-    }
+
     let dark_mode = use_context::<Signal<DarkMode>>();
     let clicked = use_context::<Signal<Clicked>>();
     use_effect(move || {
@@ -38,15 +39,11 @@ pub fn Dropdown(props: DropdownProps) -> Element {
                 on_click,
                 id: uid.to_string()
             },
-            ul{
-                class,
-                li{
-                    a{
-                        class: "dropdown-item",
-                        "item"
-                    }
-                }
-            },
+            DropdownMenu{
+                show: Some(*show.read()),
+                children: props.menu
+            }
         },
     }
 }
+
