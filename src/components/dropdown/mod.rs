f7047e67dba63a0a51720b94bf4fb5499a970985
averichev <averichev@yandex.ru>
2024-08-15 10:45:23 +0300

    improvement: Update

--- src/components/dropdown/mod.rs
+++ src/components/dropdown/mod.rs
@@ -19,52 +19,29 @@ pub fn Dropdown(props: DropdownProps) -> Element {
     let uid = use_uid_generator();
     let mut show = use_signal(|| false);
     let uid_string = uid.to_string();
+    let uid_string_func = uid_string.clone();
     let mut listener = use_document_click_listener();
     use_hook(move || {
         let cur_scope = current_scope_id().unwrap();
         let rt = Runtime::current().unwrap();
         listener.add_listener(Box::new(move |id: Option<String>| {
             rt.on_scope(cur_scope, || {
-                debug!("Updating resources!!");
-                debug!("{:?}", id);
-                if id == None {
-                    debug!("id is None {:?}", id);
-                    if *show.read() == true {
-                        debug!("is Opened");
-                        show.toggle();
+                match id {
+                    None => {
+                        if *show.read() == true {
+                            *show.write() = false;
+                        }
+                    }
+                    Some(s) => {
+                        if uid_string_func != s {
+                            *show.write() = false;
+                        }
                     }
                 }
             })
         }));
     });
-
-    let cb = use_callback(move || {
-        debug!("use_callback");
-    });
-
-
-    // let cb = {
-    //     let mut show = show.clone();
-    //     let callback_uid = uid_string.clone();
-    // };
-
-    // Оборачиваем вызов колбэка в замыкание, которое передается в `use_document_click_listener`
-    // use_document_click_listener({
-    //     move |id| {
-    //         debug!("handler {:?}", id);
-    //         // use_callback(move || {
-    //         //     debug!("use_callback {:?}", id);
-    //         // }).call(); // этот код работает
-    //         cb.call();
-    //         //state.handler(id).call(); // вот этот код вызывает панику
-    //     }
-    // });
-
-
-
-
     let on_click = move |_| {
-        debug!("on click");
         show.toggle();
     };
 
