98b0d7cc8bc4aa9788c25891c6bcecb6f845be9a
averichev <averichev@yandex.ru>
2024-08-23 14:27:25 +0300

    improvement: Update

---
+++ src/components/form/form_control.rs
@@ -0,0 +1,108 @@
+use dioxus::prelude::*;
+use crate::prelude::*;
+use tracing::debug;
+
+#[derive(Clone, PartialEq, Props)]
+pub struct FormControlProps {
+    r#type: Option<FormControlType>,
+    size: Option<FormControlSize>,
+    placeholder: Option<String>,
+    id: Option<String>,
+    name: Option<String>,
+    oninput: EventHandler<FormEvent>,
+    errors: Option<Vec<FormControlError>>,
+}
+
+
+#[component]
+pub fn FormControl(props: FormControlProps) -> Element {
+    let mut data = use_signal(|| "".to_string());
+    let is_invalid = match props.errors {
+        None => {
+            false
+        }
+        Some(ref v) => {
+            v.len() > 0
+        }
+    };
+    rsx! {
+        input {
+            r#type: get_type(props.r#type),
+            class: get_class(props.size, is_invalid),
+            placeholder: props.placeholder,
+            id: props.id,
+            name: props.name,
+            oninput: move |event| props.oninput.call(event)
+        }
+        InvalidFeedback{
+            errors: props.errors
+        }
+        div{
+            {data}
+        }
+    }
+}
+
+#[derive(Clone, PartialEq)]
+pub enum FormControlType {
+    Text,
+    Checkbox,
+}
+
+
+fn get_class(control_size: Option<FormControlSize>, is_invalid: bool) -> String {
+    let mut class = String::from("form-control");
+    match control_size {
+        None => {}
+        Some(n) => {
+            match n {
+                FormControlSize::Sm => {
+                    class.push_str(" form-control-sm")
+                }
+                FormControlSize::Default => {}
+                FormControlSize::Lg => {
+                    class.push_str(" form-control-lg")
+                }
+            }
+        }
+    };
+
+    if is_invalid == true {
+        class.push_str(" is-invalid")
+    }
+
+    class
+}
+
+fn get_type(control_type: Option<FormControlType>) -> String {
+    let t = match control_type {
+        None => {
+            "text"
+        }
+        Some(n) => {
+            match n {
+                FormControlType::Text => {
+                    "text"
+                }
+                FormControlType::Checkbox => {
+                    "checkbox"
+                }
+            }
+        }
+    };
+    t.to_string()
+}
+
+
+#[derive(Clone, PartialEq)]
+pub enum FormControlSize {
+    Sm,
+    Default,
+    Lg,
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct FormControlError {
+    pub code: String,
+    pub message: String,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
 #![allow(non_snake_case)]
 pub mod components;
 pub mod layout;
-mod models;
+pub(crate) mod models;
 pub mod hooks;
 
 pub mod prelude {
     pub use crate::layout::row::*;
     pub use crate::layout::col::*;
     pub use crate::components::form::*;
+    pub use crate::components::form::form_control::*;
+    pub use crate::components::form::form_field::*;
+    pub use crate::components::form::invalid_feedback::*;
+    pub use crate::components::form::form::*;
     pub use crate::components::dropdown::*;
     pub use crate::components::button::*;
+    pub use crate::hooks::uid_generator::*;
+    pub use crate::hooks::document_click_listener::*;
 }
